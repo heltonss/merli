be27374f700bae40908accfc9c14ae54b921e8af
helton souza silveira <helton.silveira@ltmfidelidade.com.br>
2019-09-08 20:41:06 -0300

    change - header

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
+import { Header } from 'containers/Header/Header';
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from 'store';
 import './App.scss';
 import Routes from './routes/Index';
 
 const App: React.FC = () => {
   return (
-    <>
+    <Provider store={store}>
       <main className="main">
+        <Header />
         <div className="main--navigation">
           <Routes />
         </div>
       </main>
-    </>
+    </Provider>
   );
 };
 
--- src/routes/Index.tsx
+++ src/routes/Index.tsx
@@ -2,24 +2,19 @@ import Breadcrumbs from 'components/Breadcrumbs/Breadcrumbs';
 import { Header } from 'containers/Header/Header';
 import Home from 'pages/Home/Home';
 import React, { lazy, Suspense } from 'react';
-import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import store from 'store';
 
 const Detail = lazy(() => import('pages/Detail/Detail'));
 
 const index = () => {
   return (
-    <Provider store={store}>
-      <Header />
-      <Router>
-        <Breadcrumbs />
-        <Route path="/" exact={true} component={Home} />
-        <Suspense fallback={<div>Loading...</div>}>
-          <Route path="/items/:id" component={Detail} />
-        </Suspense>
-      </Router>
-    </Provider>
+    <Router>
+      <Breadcrumbs />
+      <Route path="/" exact={true} component={Home} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Route path="/items/:id" component={Detail} />
+      </Suspense>
+    </Router>
   );
 };
 
