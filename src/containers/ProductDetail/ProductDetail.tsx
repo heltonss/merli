678f011b0ef28b80cf07520275de1af9e897f780
helton souza silveira <helton.silveira@ltmfidelidade.com.br>
2019-09-08 21:59:40 -0300

    add - layout of description

--- src/containers/ProductDetail/ProductDetail.tsx
+++ src/containers/ProductDetail/ProductDetail.tsx
@@ -3,11 +3,13 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators, Dispatch } from 'redux';
 import { IApplicationState } from 'store';
+import { IProductDescription } from 'store/ducks/product_description';
 import { IProductDetail } from 'store/ducks/product_detail';
 import { Creators as getProductDetail } from 'store/ducks/product_detail';
 import Style from './ProductDetail.module.scss';
 interface IStateToProps {
   product: IProductDetail;
+  description: IProductDescription;
 }
 
 interface IDispatchToProps {
@@ -26,27 +28,34 @@ class ProductDetail extends Component<props> {
   }
 
   render() {
-    const { product } = this.props;
+    const { product, description } = this.props;
     const { title, sold_quantity, condition, pictures, price } = product;
-    console.log('prod', product);
+    const { plain_text } = description;
     return (
-      product && (
+      <>
         <div className={Style.container}>
-          <div className={Style.image}>
-            <img src={pictures[0].url} alt={title} />
+          <div className={Style.product}>
+            <div className={Style.image}>
+              <img src={pictures[0].url} alt={title} />
+            </div>
+            <Buy
+              price={price}
+              nameProduct={title}
+              solds={sold_quantity ? `${condition} - ${sold_quantity}` : '0 vendidos'}
+            />
           </div>
-          <Buy
-            price={price}
-            nameProduct={title}
-            solds={sold_quantity ? `${condition} - ${sold_quantity}` : '0 vendidos'}
-          />
+          <div className={Style.description}>
+            <h2 className={Style.subtitle}>Description del producto</h2>
+            <p className={Style.text}> {plain_text} </p>
+          </div>
         </div>
-      )
+      </>
     );
   }
 }
 
 const mapStateToProps = (state: IApplicationState) => ({
+  description: state.productDetailDescription.data,
   product: state.productDetail.data,
 });
 
